4450f66fbb77c3afaf2740cd4d95cceec8bef111
crombus <pkundra@redhat.com>
2020-01-05 22:29:03 +0530

    Add function excercise of python

--- Ex_Files_Learning_Python/Exercise Files/Ch2/functions_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch2/functions_start.py
@@ -4,15 +4,21 @@
 
 # define a basic function
 
-
+def func1():
+    print("i am a function")
 # function that takes arguments
 
-
+def func2(arg1, arg2):
+    print(arg1," ",arg2)
 # function that returns a value
 
-
+def cube(arg):
+    return arg*arg*arg
 # function with default value for an argument
 
 
 #function with variable number of arguments
 
+func2(10,29)
+print(func2(10,20))
+print(cube(3))
