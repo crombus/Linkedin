92086767548b2b9ad97d399134cb3d0d48e48db8
crombus <pkundra@redhat.com>
2020-01-05 22:32:21 +0530

    Add conditionals to excercise

--- Ex_Files_Learning_Python/Exercise Files/Ch2/conditionals_start.py
+++ Ex_Files_Learning_Python/Exercise Files/Ch2/conditionals_start.py
@@ -8,7 +8,15 @@ def main():
   # conditional flow uses if, elif, else  
 
   # conditional statements let you use "a if C else b"
-  
+  x, y = 10 , 100
+
+  if(x<y):
+    st = "hello"
+
+  print(st)
+
+  stw = "wor"  if (x<y) else "yuhb" 
+  print(stw)
 
 if __name__ == "__main__":
   main()
